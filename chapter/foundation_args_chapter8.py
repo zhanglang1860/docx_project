8c0effa3c65149d8e8b8ba494f67ab25137f27e1
zhanglang1860 <42694007+zhanglang1860@users.noreply.github.com>
2019-02-12 09:57:27 +0800

    dox_project: 1.chapter 5 dynamic table and images  finished start chapter 8 almost 100%

--- chapter/foundation_args_chapter8.py
+++ chapter/foundation_args_chapter8.py
@@ -1,21 +1,41 @@
 def foundation_args_chapter8(**kwargs):
+    """
+    :param: dict 载入需要查询的字典key，以及对应的value。例如：
+    args_chapter8 = {'foundation_type': '扩展基础', 'max_load': 110000}
+    :returns: str, str foundation_sql, key_value
+    SELECT * FROM foundation_model WHERE %s = '%s' and %s = '%s'
+    key_value ['foundation_type', '扩展基础', 'maxload', 110000]
+
+    """
+
     sql_str = ''
-    key_vaule = []
+    key_value = []
     for k, v in kwargs.items():
         if sql_str == '':
             sql_str = sql_str + '%s' + ' = ' + '\'%s\''
         else:
             sql_str = sql_str + ' and ' + '%s' + ' = ' + '\'%s\''
-        key_vaule.append(k)
-        key_vaule.append(v)
+        key_value.append(k)
+        key_value.append(v)
     foundation_sql = "SELECT * FROM foundation_model WHERE " + sql_str
-    return foundation_sql, key_vaule
+    return foundation_sql, key_value
 
-def p(*key_vaule):
-    return sql_foundation%key_vaule
 
-args_chapter8 = {'foundation_type': '扩展基础', 'maxload': 110000}
-sql_foundation, key_vaule = foundation_args_chapter8(**args_chapter8)
-print(sql_foundation,key_vaule)
-sql_foundation=p(*key_vaule)
-print(sql_foundation)
+def present(sql_foundation, *key_value):
+    """
+    :param key_value:
+    :return: str sql 一条sql查询语句
+    SELECT * FROM foundation_model WHERE
+     foundation_type = '扩展基础'and maxload = '110000'
+
+    """
+    return sql_foundation % key_value
+
+# ****************
+#   测试调用
+# args_chapter8 = {'foundation_type': '扩展基础', 'maxload': 110000}
+# sql_foundation, key_value = foundation_args_chapter8(**args_chapter8)
+# print(sql_foundation,key_value)
+# sql_foundation=present(sql_foundation,*key_value)
+# print(sql_foundation)
+# ****************
--- chapter/generate_docx.py
+++ chapter/generate_docx.py
@@ -4,63 +4,90 @@ from docxtpl import DocxTemplate, InlineImage
 from foundation_args_chapter8 import foundation_args_chapter8
 from connect_sql import connect_sql_chapter5, connect_sql_chapter8
 
-path_images = r"C:\Users\Administrator\PycharmProjects\docx_project\files\results"
+# **********************************************
+print("*" * 30)
+# step:1
+# 载入参数
+print("---------step:1  载入参数--------")
 turbine_list = ['GW3.3-155', 'MY2.5-145', 'GW3.0-140', 'GW3.4-140', 'GW2.5-140']
-
 args_chapter8 = {'foundation_type': '扩展基础', 'max_load': 110000}
-print(type(args_chapter8))
-
-args_chapter8 = {'foundation_type': '扩展基础', 'maxload': 110000}
-
-tur_np, power_np, efficiency_np = connect_sql_chapter5(*turbine_list)
-# sql_foundation, key_vaule = foundation_args_chapter8(foundation_type='扩展基础', baseboard_r=11)
+path_images = r"C:\Users\Administrator\PycharmProjects\docx_project\files\results"
+print("机型选型：" + str(turbine_list))
+print("基础选择：" + str(args_chapter8))
+print("图片成路径：" + path_images)
+print("---------step:1  载入载入完毕--------")
+#
+# **********************************************
+print("*" * 30)
+# step:2
+# 生成数组
+print("---------step:2  生成数组--------")
+tur_np, power_np, efficiency_np = connect_sql_chapter5(*turbine_list)  # 一会儿注释connect_sql_chapter5
 sql_foundation, key_vaule = foundation_args_chapter8(**args_chapter8)
 foundation_np = connect_sql_chapter8(sql_foundation, *key_vaule)
-generate_images(path_images, power_np, efficiency_np)
+print("机型选型数组：" + str(tur_np))
+print("功率曲线数组：" + str(power_np))
+print("机型效率数组：" + str(efficiency_np))
+print("---------step:2  生成数组完毕--------")
 
+# **********************************************
+print("*" * 30)
+# step:3
+# 生成图片
+print("---------step:3  生成图片--------")
+generate_images(path_images, power_np, efficiency_np)  # 一会儿注释generate_images
+print("---------step:3  生成图片完毕--------")
 
+# **********************************************
+print("*" * 30)
+# step:4
+# 生成报告
+# **********************************************
+print("*" * 30)
+print("---------step:4  生成报告--------")
+print("---------开始 chapter 5--------")
 # chapter 5
-# tpl = DocxTemplate(r'C:\Users\Administrator\PycharmProjects\docx_project\files\CR_chapter5_template.docx')
-# context = {}
-# for i in range(0, 16):
-#     if i < 13:
-#         key = 'tbl_contents' + str(i)
-#         value = tur_np[:, i + 1]
-#         context[key] = value
-#     else:
-#         key = 'tbl_contents' + str(i)
-#         value = tur_np[:, i + 4]
-#         context[key] = value
-# print(context)
-#
-# png_box = ('powers', 'efficiency')
-# for i in range(0, 2):
-#     key = 'myimage' + str(i)
-#     value = InlineImage(tpl, os.path.join(path_images, '%s.png') % png_box[i])
-#     context[key] = value
-# tpl.render(context)
-# tpl.save(r'C:\Users\Administrator\PycharmProjects\docx_project\files\results\result_chapter5.docx')
-# print(foundation_np)
+tpl = DocxTemplate(r'C:\Users\Administrator\PycharmProjects\docx_project\files\CR_chapter5_template.docx')
+context = {}
+for i in range(0, 16):
+    if i < 13:
+        key = 'tbl_contents' + str(i)
+        value = tur_np[:, i + 1]
+        context[key] = value
+    else:
+        key = 'tbl_contents' + str(i)
+        value = tur_np[:, i + 4]
+        context[key] = value
+print(context)
 
-# # chapter 8
+png_box = ('powers', 'efficiency')
+for i in range(0, 2):
+    key = 'myimage' + str(i)
+    value = InlineImage(tpl, os.path.join(path_images, '%s.png') % png_box[i])
+    context[key] = value
+tpl.render(context)
+tpl.save(r'C:\Users\Administrator\PycharmProjects\docx_project\files\results\result_chapter5.docx')
+print("---------chapter 5 生成完毕--------")
+# **********************************************
+print("*" * 30)
+print("---------开始 chapter 8--------")
 
+
+#  chapter 8
 def Dict_chapter_8(foundation_np):
-    import numpy as np
-    np.set_printoptions(precision=2)
     Dict_8 = {}
     keys = ['ID', '基础型式', '极限荷载', '设防烈度', '承载力', '土方比', '底板半径R', '棱台顶面半径R1', '台柱半径R2',
             '底板外缘高度H1', '底板棱台高度H2', '台柱高度H3', '桩直径', '根数', '长度', '总桩长', '面积m2', '体积m3',
             '垫层', '土方开挖', '石方开挖', '土石方回填', '复合地基换填', '复合地基桩']
     for i in range(0, len(keys)):
-        key = keys[i]
-        value = foundation_np[:, i][0]
+        key_foundation = keys[i]
+        value_foundation = foundation_np[:, i][0]
 
-        Dict_8[key] = value
+        Dict_8[key_foundation] = value_foundation
     return Dict_8
 
 
 Dict_8 = Dict_chapter_8(foundation_np)
-print(Dict_8)
 
 keys = ['土方开挖', '石方开挖', '土石方回填', '体积m3', '垫层', '钢筋', '基础防水', '沉降观测']
 
@@ -71,14 +98,16 @@ def Write_Dict_chapter_8(Dict_8, *args):
         key = i
         value = Dict_8.get(i, None)
         context[key] = value
-    context['钢筋'] = round(Dict_8['体积m3'] / 10,2)
+        if value != None:
+            context[key] = round(Dict_8[key], 2)
+
+    context['钢筋'] = round(Dict_8['体积m3'] / 10, 3)
     context['基础防水'] = 1
     context['沉降观测'] = 4
     return context
 
 
 context = Write_Dict_chapter_8(Dict_8, *keys)
-print(context)
 
 tpl = DocxTemplate(r'C:\Users\Administrator\PycharmProjects\docx_project\files\CR_chapter8_template.docx')
 tpl.render(context)
