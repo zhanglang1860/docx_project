4184d552d8dd935ea2c1bba7b1ff5a1d3309606c
zhanglang1860 <42694007+zhanglang1860@users.noreply.github.com>
2019-02-12 15:32:10 +0800

    dox_project: 1.chapter 5 dynamic table and images  finished chapter 8 100% prepare annotation and class

---
+++ chapter/generate_dict.py
@@ -0,0 +1,95 @@
+#  chapter 8
+def get_dict(np, dict_keys):
+    """
+    结合传入np数组和dict_keys关键字参数，创建字典。注意一一对应。
+    :param np:  传入数组
+    :param dict_keys: 关键字字典
+    :return: 返回字典
+
+    """
+    dict = {}
+    for i in range(0, len(dict_keys)):
+        key_dict = dict_keys[i]
+        value_dict = np[:, i]
+        dict[key_dict] = value_dict
+    return dict
+
+
+def write_context(dict, *context_keys):
+    """
+    根据context_keys的内容，找出dict内部的keys-value，并形成context，用以docx-temple
+    :param dict: 输入的字典
+    :param context_keys: context的特定值
+    :return: 返回context字典
+    """
+    context = {}
+    for i in context_keys:
+        key = i
+        value = dict.get(i, None)
+        context[key] = value
+    return context
+
+
+def write_context_numbers(dict, *context_keys, numbers):
+    """
+    根据context_keys的内容，找出dict内部的keys-value，并形成context，用以docx-temple
+    :param dict: 输入的字典
+    :param context_keys: context的特定值
+    :param numbers: 风机台数
+    :return: 返回context字典
+    """
+    context = {}
+    context['numbers'] = numbers
+    for i in context_keys:
+        key = i
+        key_numbers = i + 'numbers'
+        value = dict.get(i, None)
+        if value != None:
+            value = float('%.02f' % dict[key])
+            value_numbers = float('%.02f' % (dict[key] * numbers))
+        else:
+            value = None
+            value_numbers = None
+        context[key] = value
+        context[key_numbers] = value_numbers
+    return context
+
+
+# ****************
+#   测试调用
+# from foundation_args_chapter8 import foundation_args_chapter8
+# from connect_sql import connect_sql_chapter5, connect_sql_chapter8
+# from connect_sql import connect_sql_chapter5, connect_sql_chapter8
+
+# chapter 5
+# turbine_list = ['GW3.3-155', 'MY2.5-145', 'GW3.0-140', 'GW3.4-140', 'GW2.5-140']
+# tur_np, power_np, efficiency_np = connect_sql_chapter5(*turbine_list)  # 一会儿注释connect_sql_chapter5
+# print(tur_np)
+# dict_keys_chapter5 = ['型号ID', '机组类型', '功率', '叶片数', '风轮直径', '扫风面积', '轮毂高度',
+#                       '功率调节', '切入风速', '切出风速', '额定风速', '发电机型式', '额定功率', '电压', '频率',
+#                       '塔架型式', '塔筒重量', '主制动系统', '第二制动', '3秒最大值']
+# context_keys_chapter5 = ['机组类型', '机组类型', '叶片数', '风轮直径', '扫风面积', '轮毂高度', '功率调节', '切入风速',
+#                          '切出风速', '额定风速', '发电机型式', '额定功率', '电压', '主制动系统', '第二制动']
+# Dict_5 = get_dict(tur_np, dict_keys_chapter5)
+# context = write_context(Dict_5, *context_keys_chapter5)
+# print(context)
+# chapter 8
+# args_chapter8 = {'foundation_type': '扩展基础', 'max_load': 110000}
+# sql_foundation, key_vaule = foundation_args_chapter8(**args_chapter8)
+# foundation_np = connect_sql_chapter8(sql_foundation, *key_vaule)
+#
+# dict_keys = ['ID', '基础型式', '极限荷载', '设防烈度', '承载力', '土方比', '底板半径R', '棱台顶面半径R1', '台柱半径R2',
+#              '底板外缘高度H1', '底板棱台高度H2', '台柱高度H3', '桩直径', '根数', '长度', '总桩长', '面积m2', '体积m3',
+#              '垫层', '土方开挖', '石方开挖', '土石方回填', '复合地基换填', '复合地基桩']
+# context_keys = ['土方开挖', '石方开挖', '土石方回填', '体积m3', '垫层', '钢筋', '基础防水', '沉降观测']
+# numbers = 32
+#
+# Dict_8 = get_dict(foundation_np, dict_keys)
+# context = write_context_numbers(Dict_8, *context_keys, numbers=numbers)
+# context['钢筋'] = float('%.02f' % (Dict_8['体积m3'] / 10))
+# context['钢筋numbers'] = float('%.02f' % (Dict_8['体积m3'] / 10 * numbers))
+# context['基础防水'] = 1
+# context['基础防水numbers'] = 1 * numbers
+# context['沉降观测'] = 4
+# context['沉降观测numbers'] = 4 * numbers
+# print(context)
--- chapter/generate_docx.py
+++ chapter/generate_docx.py
@@ -2,6 +2,7 @@ import os
 from generate_images import generate_images
 from docxtpl import DocxTemplate, InlineImage
 from foundation_args_chapter8 import foundation_args_chapter8
+from generate_dict import get_dict, write_context_numbers,write_context
 from connect_sql import connect_sql_chapter5, connect_sql_chapter8
 
 # **********************************************
@@ -9,12 +10,24 @@ print("*" * 30)
 # step:1
 # 载入参数
 print("---------step:1  载入参数--------")
-turbine_list = ['GW3.3-155', 'MY2.5-145', 'GW3.0-140', 'GW3.4-140', 'GW2.5-140']
-args_chapter8 = {'foundation_type': '扩展基础', 'max_load': 110000}
+#  chapter 5
+args_chapter5 = ['GW3.3-155', 'MY2.5-145', 'GW3.0-140', 'GW3.4-140', 'GW2.5-140']
 path_images = r"C:\Users\Administrator\PycharmProjects\docx_project\files\results"
-print("机型选型：" + str(turbine_list))
-print("基础选择：" + str(args_chapter8))
-print("图片成路径：" + path_images)
+dict_keys_chapter5 = ['型号ID', '机组类型', '功率', '叶片数', '风轮直径', '扫风面积', '轮毂高度',
+                      '功率调节', '切入风速', '切出风速', '额定风速', '发电机型式', '额定功率', '电压', '频率',
+                      '塔架型式', '塔筒重量', '主制动系统', '第二制动', '三秒最大值']
+context_keys_chapter5 = ['机组类型', '功率', '叶片数', '风轮直径', '扫风面积', '轮毂高度', '功率调节', '切入风速',
+                         '切出风速', '额定风速', '发电机型式', '额定功率', '电压', '主制动系统', '第二制动','三秒最大值']
+#  chapter 8
+args_chapter8 = {'foundation_type': '预制桩承台基础', 'max_load': 100000}
+dict_keys_chapter8 = ['ID', '基础型式', '极限荷载', '设防烈度', '承载力', '土方比', '底板半径R', '棱台顶面半径R1', '台柱半径R2',
+                      '底板外缘高度H1', '底板棱台高度H2', '台柱高度H3', '桩直径', '根数', '长度', '总桩长', '面积m2', '体积m3',
+                      '垫层', '土方开挖', '石方开挖', '土石方回填', '复合地基换填', '复合地基桩']
+context_keys_chapter8 = ['土方开挖', '石方开挖', '土石方回填', '体积m3', '垫层', '钢筋', '基础防水', '沉降观测']
+numbers = 20  # 风机个数
+print("机型选型：" + str(args_chapter5))
+print("基础选择参数：" + str(args_chapter8))
+print("风机数量：" + str(numbers))
 print("---------step:1  载入载入完毕--------")
 #
 # **********************************************
@@ -22,7 +35,7 @@ print("*" * 30)
 # step:2
 # 生成数组
 print("---------step:2  生成数组--------")
-tur_np, power_np, efficiency_np = connect_sql_chapter5(*turbine_list)  # 一会儿注释connect_sql_chapter5
+tur_np, power_np, efficiency_np = connect_sql_chapter5(*args_chapter5)  # 一会儿注释connect_sql_chapter5
 sql_foundation, key_vaule = foundation_args_chapter8(**args_chapter8)
 foundation_np = connect_sql_chapter8(sql_foundation, *key_vaule)
 print("机型选型数组：" + str(tur_np))
@@ -49,80 +62,30 @@ print("---------开始 chapter 5--------")
 # chapter 5
 tpl = DocxTemplate(r'C:\Users\Administrator\PycharmProjects\docx_project\files\CR_chapter5_template.docx')
 context = {}
-for i in range(0, 16):
-    if i < 13:
-        key = 'tbl_contents' + str(i)
-        value = tur_np[:, i + 1]
-        context[key] = value
-    else:
-        key = 'tbl_contents' + str(i)
-        value = tur_np[:, i + 4]
-        context[key] = value
-print(context)
-
+Dict_5 = get_dict(tur_np, dict_keys_chapter5)
+context_5 = write_context(Dict_5, *context_keys_chapter5)
 png_box = ('powers', 'efficiency')
 for i in range(0, 2):
     key = 'myimage' + str(i)
     value = InlineImage(tpl, os.path.join(path_images, '%s.png') % png_box[i])
-    context[key] = value
-tpl.render(context)
+    context_5[key] = value
+tpl.render(context_5)
 tpl.save(r'C:\Users\Administrator\PycharmProjects\docx_project\files\results\result_chapter5.docx')
 print("---------chapter 5 生成完毕--------")
 # **********************************************
 print("*" * 30)
 print("---------开始 chapter 8--------")
 
+Dict_8 = get_dict(foundation_np, dict_keys_chapter8)
+context_8 = write_context_numbers(Dict_8, *context_keys_chapter8, numbers=numbers)
+context['钢筋'] = float('%.02f' % (Dict_8['体积m3'] / 10))
+context['钢筋numbers'] = float('%.02f' % (Dict_8['体积m3'] / 10 * numbers))
+context['基础防水'] = 1
+context['基础防水numbers'] = 1 * numbers
+context['沉降观测'] = 4
+context['沉降观测numbers'] = 4 * numbers
 
-#  chapter 8
-def Dict_chapter_8(foundation_np):
-    Dict_8 = {}
-    keys = ['ID', '基础型式', '极限荷载', '设防烈度', '承载力', '土方比', '底板半径R', '棱台顶面半径R1', '台柱半径R2',
-            '底板外缘高度H1', '底板棱台高度H2', '台柱高度H3', '桩直径', '根数', '长度', '总桩长', '面积m2', '体积m3',
-            '垫层', '土方开挖', '石方开挖', '土石方回填', '复合地基换填', '复合地基桩']
-    for i in range(0, len(keys)):
-        key_foundation = keys[i]
-        value_foundation = foundation_np[:, i][0]
-
-        Dict_8[key_foundation] = value_foundation
-    return Dict_8
-
-
-Dict_8 = Dict_chapter_8(foundation_np)
-print('****')
-print(Dict_8)
-
-keys = ['土方开挖', '石方开挖', '土石方回填', '体积m3', '垫层', '钢筋', '基础防水', '沉降观测']
-numbers = 32
-
-
-def Write_Dict_chapter_8(Dict_8, *args, numbers):
-    context = {}
-    context['numbers'] = numbers
-    for i in args:
-        key = i
-        key2 = i + 'numbers'
-        value = Dict_8.get(i, None)
-        if value != None:
-            value = float('%.02f' % Dict_8[key])
-            value2 = float('%.02f'% (Dict_8[key] * numbers))
-        else:
-            value = None
-            value2 = None
-        context[key] = value
-        context[key2] = value2
-
-    context['钢筋'] = float('%.02f' % (Dict_8['体积m3'] / 10))
-    context['钢筋numbers'] = float('%.02f' % (Dict_8['体积m3'] / 10 * numbers))
-    context['基础防水'] = 1
-    context['基础防水numbers'] = 1 * numbers
-    context['沉降观测'] = 4
-    context['沉降观测numbers'] = 4 * numbers
-
-    return context
-
-
-context = Write_Dict_chapter_8(Dict_8, *keys, numbers=32)
-print(context)
 tpl = DocxTemplate(r'C:\Users\Administrator\PycharmProjects\docx_project\files\CR_chapter8_template.docx')
-tpl.render(context)
+tpl.render(context_8)
 tpl.save(r'C:\Users\Administrator\PycharmProjects\docx_project\files\results\result_chapter8.docx')
+print("---------chapter 8 生成完毕--------")
