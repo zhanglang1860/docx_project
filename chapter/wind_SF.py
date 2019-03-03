52b2a6a5c1a10b4e1c4649b0ddf9d7c0eb3376d4
zhanglang1860 <42694007+zhanglang1860@users.noreply.github.com>
2019-03-03 11:10:56 +0800

    base finished chapter 6 80%

--- chapter/wind_SF.py
+++ chapter/wind_SF.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 basestation = r"C:\Users\Administrator\PycharmProjects\docx_project\chapter\风速风频.xlsx"
-basestation1 = r"C:\Users\Administrator\PycharmProjects\docx_project\chapter\风速风频1.xlsx"
+basestation1 = r"C:\Users\Administrator\PycharmProjects\docx_project\chapter\风速风频1.txt"
 
 data = pd.read_excel(basestation)
 
@@ -60,12 +60,39 @@ def ti(x):
     return (x + statistics_ti['sd_sd'].std() * 1.28)
 
 
-result = [i for i in range(0, 20)]
-for i in range(0, 20):
-    statistics_ti = data.loc[data['wind_speed'] == i+1]
-    print(i,statistics_ti['sd_sd'].mean(),statistics_ti['sd_sd'].std() * 1.28)
-    result[i] = statistics_ti['sd_sd'].mean() + statistics_ti['sd_sd'].std() * 1.28
-
 statistics_times_times['frequency'] = statistics_times_times['times'].map(frequency)
+print(statistics_times_times)
+npa = statistics_times_times[((statistics_times_times['frequency']) > 0.1)]
+npa_np = np.array(npa[['sector', 'frequency']])
+print(npa_np)
+result = [i for i in range(0, 21)]
+result_np = [i for i in range(0, 21)]
+for i in range(0, 21):
+    statistics_ti = data.loc[data['wind_speed'] == i + 1]
+    # print(i, statistics_ti['sd_sd'].mean(), statistics_ti['sd_sd'].std() * 1.28)
+    print("```````````````````")
+    statistics_ti_np = np.array(statistics_ti[['sector', 'sd_sd']])
+    print("```````````````````")
+    statistics_ti_np_f = []
+    for j in range(0, len(statistics_ti_np[:, 1])):
+        if statistics_ti_np[j, 1] > 0.2 and (statistics_ti_np[j, 0] not in npa_np[:, 0]):
+            statistics_ti_np[j, 1] = None
+        else:
+            statistics_ti_np_f.append(statistics_ti_np[j, 1])
 
-print(result)
+    statistics_ti_np_f = np.array(statistics_ti_np_f)
+    print("*******************")
+    print("statistics_ti_np" + str(len(statistics_ti_np)))
+    print("statistics_ti_np_f"+str(len(statistics_ti_np_f)))
+    # statistics_ti = statistics_ti[(statistics_ti['sd_sd'] > 0.2 and np['sector'].isin([statistics_ti['sector']]))]
+
+    # print(statistics_ti_np)
+
+    # result[i] = statistics_ti['sd_sd'].mean() + statistics_ti['sd_sd'].std() * 1.28
+    result_np[i] = statistics_ti_np_f.mean() + statistics_ti_np_f.std() * 1.28
+    print(result_np[i])
+f1 = open(basestation1, 'w')
+f1.write(str(result_np))
+f1.close()
+print()
+print("-----------数组写入Excel完成-------------")
